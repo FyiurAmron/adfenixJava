d5f5fd0c61254f98e5c94aa0f906d74bd9e3b616
FyiurAmron <spamove@gmail.com>
2022-05-27 20:39:34 +0200

    minor tweaks

--- src/main/java/se/quedro/challenge/data/json/SaleObjectGroup.java
+++ src/main/java/se/quedro/challenge/data/json/SaleObjectGroup.java
@@ -1,9 +1,9 @@
 package se.quedro.challenge.data.json;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public record SaleObjectGroup(
     int numberOfSaleObjects,
-    List<SaleObject> saleObjects
+    ArrayList<SaleObject> saleObjects
 ) {
 }
