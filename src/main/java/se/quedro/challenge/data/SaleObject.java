04b43a413bf895ff63030af440b609486b753733
FyiurAmron <spamove@gmail.com>
2022-05-27 18:23:58 +0200

    cleanup of the docs in consumer API  i.e. translated them to an actual human-readable language

--- src/main/java/se/quedro/challenge/data/SaleObject.java
+++ src/main/java/se/quedro/challenge/data/SaleObject.java
@@ -7,6 +7,7 @@ public record SaleObject(
     SaleObjectType type,
     int sizeSqm,
     int startingPrice,
+    int startingPricePerSquareMeter,
     Address address
 ) {
     public static List<SaleObject> listFrom( se.quedro.challenge.data.json.SaleObjectGroup saleObjectGroup ) {
@@ -23,22 +24,34 @@ public record SaleObject(
         return saleObjectGroup.saleObjects().stream().map( SaleObject::from ).toList();
     }
 
+    private static int calcStartingPricePerSquareMeter( int startingPrice, int sizeSqm ) {
+        return startingPrice * 1000 / sizeSqm;
+    }
+
     public static SaleObject from( se.quedro.challenge.data.json.SaleObject saleObject ) {
+        int sizeSqm = Integer.parseInt( saleObject.sizeSqm() );
+        int startingPrice = saleObject.startingPrice();
+
         return new SaleObject(
             saleObject.id(),
             SaleObjectType.from( saleObject.type() ),
-            Integer.parseInt( saleObject.sizeSqm() ),
-            saleObject.startingPrice(),
+            sizeSqm,
+            startingPrice,
+            calcStartingPricePerSquareMeter( startingPrice, sizeSqm ),
             Address.from( saleObject.postalAddress() )
         );
     }
 
     public static SaleObject from( se.quedro.challenge.data.xml.SaleObject saleObject ) {
+        int sizeSqm = saleObject.sizeSqm();
+        int startingPrice = Integer.parseInt( saleObject.startingPrice().replace( ".", "" ) );
+
         return new SaleObject(
             Integer.parseInt( saleObject.id() ),
             SaleObjectType.from( saleObject.type() ),
-            saleObject.sizeSqm(),
-            Integer.parseInt( saleObject.startingPrice().replace( ".", "" ) ),
+            sizeSqm,
+            startingPrice,
+            calcStartingPricePerSquareMeter( startingPrice, sizeSqm ),
             Address.from( saleObject.address() )
         );
     }
--- src/main/java/se/quedro/challenge/external/SaleObjectConsumer.java
+++ src/main/java/se/quedro/challenge/external/SaleObjectConsumer.java
@@ -2,35 +2,41 @@ package se.quedro.challenge.external;
 
 public interface SaleObjectConsumer {
     /**
-     * This method is called to decide how to order the objects before calling reportSaleObject.
+     * Gets the attribute by which to sort/order the following {@link #reportSaleObject} calls
      *
-     * @return the attribute of which to order by.
+     * @return the attribute by which to order the data sequence
      */
     PriorityOrderAttribute getPriorityOrderAttribute();
 
     /**
-     * Must be called BEFORE using the reportSaleObjects()-method.
+     * Must be called <b>before</b> using {@link #reportSaleObject}
      */
     void startSaleObjectTransaction();
 
     /**
-     * Called once per SaleObject. This method must be called with objects ordered by the attribute decided by a call to getPriorityOrderAttribute()
+     * Called once per SaleObject.
+     * Must be called in order decided by a prior call to {@link #getPriorityOrderAttribute()}
      *
      * @param squareMeters        The size of the house or apartment in square meters.
-     * @param pricePerSquareMeter The price of the house or apartment divided by square meters. The price must always have exactly three decimals and must not contain a decimal separator. So 1USD = 1000, 2075.4USD = 2075400, 1.511USD = 1511 and so on.
+     * @param pricePerSquareMeter The price of the house or apartment divided by square meters.
+     *                            The price must be in fixed-point representation with scaling 1/1000.
+     *                            Also, it's absurd since there's no indication how to sort this: it's a String,
+     *                            so it should be sorted lexicographically, but it's actually a numeric value...
      * @param city                The city where the house is located.
      * @param street              The street where the house is located.
      * @param floor               The floor if eligible, otherwise null.
-     * @throws TechnicalException if any problem occurs during the invocation resulting in the object not being reported successfully.
+     * @throws TechnicalException if an object can not be reported successfully
      */
     void reportSaleObject( int squareMeters, String pricePerSquareMeter, String city, String street, Integer floor )
         throws TechnicalException;
 
     /**
-     * Must be called AFTER reporting SaleObjects.
+     * Must be called <b>after</b> using {@link #reportSaleObject}
      */
     void commitSaleObjectTransaction();
 
+    // ??? is this ascending or descending? I'll assume ascending...
+    // ??? also, why doesn't this have such obvious value as just `Price`?
     enum PriorityOrderAttribute {
         City,
         SquareMeters,
